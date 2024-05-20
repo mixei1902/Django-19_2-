db6f2dbed52eed1e0057fbed49f728c48cdd3f51
mixei1902 <misei190289@gmail.com>
2024-05-20 19:49:52 +0300

    make migrate 0003

--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Название категории")
+    name = models.CharField(max_length=50, verbose_name="Название категории")
     description = models.TextField(verbose_name="Описание категории")
 
     def __str__(self):
@@ -14,11 +14,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Название продукта")
+    name = models.CharField(max_length=50, verbose_name="Название продукта")
     description = models.TextField(verbose_name="Описание ппродукта")
     image = models.ImageField(upload_to='products/', verbose_name="изображение", null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name="Категория продукта")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    manufactured_at = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
