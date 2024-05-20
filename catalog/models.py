5062be30a12b55f6483cdf201c185f9f4cc65246
mixei1902 <misei190289@gmail.com>
2024-05-20 19:08:44 +0300

    add models product,category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название категории")
+    description = models.TextField(verbose_name="Описание категории")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название продукта")
+    description = models.TextField(verbose_name="Описание ппродукта")
+    image = models.ImageField(upload_to='products/', verbose_name="изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='категория продукта')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
--- config/settings.py
+++ config/settings.py
@@ -106,7 +106,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIR = (BASE_DIR / 'static',)
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
