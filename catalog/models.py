f291593b97603ad33ca8207d51c58591767ff6fb
mixei1902 <misei190289@gmail.com>
2024-05-20 19:20:26 +0300

    migrate 0001

--- catalog/models.py
+++ catalog/models.py
@@ -9,15 +9,15 @@ class Category(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название продукта")
     description = models.TextField(verbose_name="Описание ппродукта")
     image = models.ImageField(upload_to='products/', verbose_name="изображение")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='категория продукта')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name="Категория продукта")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
