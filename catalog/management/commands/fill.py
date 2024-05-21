d60c112a2379266ebf8c6e8faafe7f8187fed7fc
mixei1902 <misei190289@gmail.com>
2024-05-21 09:56:29 +0300

    fix customs fill

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -6,38 +6,33 @@ from catalog.models import Category, Product
 
 
 class Command(BaseCommand):
+    help = 'Populate the database with initial data'
 
     @staticmethod
     def json_read_categories():
-        with open('fixtures/categories.json', encoding='utf-8') as file:
+        with open('fixtures/categories.json') as file:
             return json.load(file)
 
     @staticmethod
     def json_read_products():
-        with open('fixtures/products.json', encoding='utf-8') as file:
+        with open('fixtures/products.json') as file:
             return json.load(file)
 
     def handle(self, *args, **options):
-        # Удалите все продукты
         Product.objects.all().delete()
-        # Удалите все категории
         Category.objects.all().delete()
 
-        # Создайте списки для хранения объектов
         product_for_create = []
         category_for_create = []
 
-        # Обходим все значения категорий из фикстуры для получения информации об одном объекте
         for category_data in Command.json_read_categories():
             category = category_data['fields']
             category_for_create.append(
                 Category(id=category_data['pk'], name=category['name'], description=category['description'])
             )
 
-        # Создаем объекты в базе с помощью метода bulk_create()
         Category.objects.bulk_create(category_for_create)
 
-        # Обходим все значения продуктов из фикстуры для получения информации об одном объекте
         for product_data in Command.json_read_products():
             product = product_data['fields']
             product_for_create.append(
@@ -53,6 +48,5 @@ class Command(BaseCommand):
                 )
             )
 
-        # Создаем объекты в базе с помощью метода bulk_create()
         Product.objects.bulk_create(product_for_create)
         self.stdout.write(self.style.SUCCESS('Successfully populated the database'))
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
-]
+   ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
