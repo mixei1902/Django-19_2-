87891624232b45c5f1b4cca4e59dee2bd6fd9ecb
mixei1902 <misei190289@gmail.com>
2024-05-24 15:55:42 +0300

    HW 20/2 add menu +contakt

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,9 +10,12 @@
 #     path('product/<int:pk>/', product_detail, name='product_detail'),
 # ]
 from django.urls import path
+
+from . import views
 from .views import home, product_detail
 
 urlpatterns = [
     path('', home, name='home'),
     path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('contacts/', views.contact, name='contacts'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -22,14 +22,21 @@
 #         print(f'Имя: {name}\nТел: {phone}\nСообщение: {message}')
 #     return render(request, 'contacts.html')
 from django.shortcuts import render, get_object_or_404
+
 from .models import Product
+
 
 def home(request):
     products = Product.objects.all()
     context = {'object_list': products}
     return render(request, 'catalog/home.html', context)
 
+
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {'object': product}
     return render(request, 'catalog/product_detail.html', context)
+
+
+def contact(request):
+    return render(request, 'catalog/contact.html')
