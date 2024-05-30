91ebe0dcb398504a5bea16b9ddcf73208c79926c
mixei1902 <misei190289@gmail.com>
2024-05-30 17:37:26 +0300

    HW 21/1 realize CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,3 @@
-# from django.urls import path, include
-# from catalog.apps import NewappConfig
-# from catalog.views import home, contacts, product_detail
-#
-# app_name = NewappConfig.name
-#
-# urlpatterns = [
-#     path('', home, name='home'),
-#     path('contacts/', contacts, name='contacts'),
-#     path('product/<int:pk>/', product_detail, name='product_detail'),
-# ]
-from django.urls import path
-
 from django.urls import path
 from .views import HomeView, ProductDetailView, ContactView, CategoryDetailView
 
