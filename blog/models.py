54f17e624562bc9e094349ec5e86a98e1a043f67
mixei1902 <misei190289@gmail.com>
2024-05-30 20:31:12 +0300

    HW 21/1 fix

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200,verbose_name ='Заголовок'  ,blank=True )
     slug = models.CharField(max_length=200, unique=True)
     content = models.TextField(verbose_name ='Содержание')
-    preview = models.ImageField(upload_to='blog_previews/',verbose_name ='Изображение')
+    preview = models.ImageField(upload_to='previews/',verbose_name ='Изображение', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True,verbose_name ='Дата создания')
     is_published = models.BooleanField(default=False,verbose_name ='Признак публикации')
     views_count = models.PositiveIntegerField(default=0,verbose_name ='Количество просмотров')
