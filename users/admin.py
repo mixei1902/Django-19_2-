7d1e0cf55de9d2d435685bc69cd10d1d1c60987a
mixei1902 <misei190289@gmail.com>
2024-06-20 09:51:23 +0300

    HW 22.2 add product

--- users/admin.py
+++ users/admin.py
@@ -1,27 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _
 
 from users.models import User
 
 
 class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('phone', 'tg_name', 'avatar')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        ('Personal info', {'fields': ('phone', 'avatar', 'token')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active'),
         }),
     )
-    list_display = ('email', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active', 'is_superuser')
-    search_fields = ('email',)
-    ordering = ('email',)
 
 
 admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,30 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Поле почта должно быть заполнено')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     username = None
     email = models.EmailField(unique=True, verbose_name='Email')
     phone = models.CharField(max_length=35, verbose_name='номер телефона', blank=True, null=True,
@@ -10,8 +32,13 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/avatars', null=True, blank=True, verbose_name='Аватар',
                                help_text='Загрузите фотографию')
     token = models.CharField(max_length=100, verbose_name='token', blank=True, null=True)
+    is_staff = models.BooleanField(default=False, verbose_name='staff status')
+    is_active = models.BooleanField(default=True, verbose_name='active')
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     class Meta:
         verbose_name = "Пользователь"
